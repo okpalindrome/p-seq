d0fe178d14f408d868515aecc86ad16cf81d9947
okpalindrome <okpalindrome@Nitins-MacBook-Pro.local>
2026-06-01 00:44:20 +0530

    windows: py env patch

--- run.bat
+++ run.bat
@@ -5,14 +5,9 @@ cd /d "%~dp0"
 set "VENV_PY=.venv\Scripts\python.exe"
 
 if not exist "%VENV_PY%" (
-    where py >nul 2>nul
-    if %ERRORLEVEL% EQU 0 (
-        py -3 -m venv .venv
-    ) else (
-        python -m venv .venv
-    )
+    python -m venv .venv
     if not exist "%VENV_PY%" (
-        echo Failed to create virtualenv. Make sure Python 3 is installed and on PATH.
+        echo Failed to create virtualenv. Make sure Python 3 is installed and on PATH ^(try: python --version^).
         exit /b 1
     )
     "%VENV_PY%" -m pip install --upgrade pip
